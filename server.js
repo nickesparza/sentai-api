12c7dd72f2bc93b7078800021b5d8bf8e2933b52
nickesparza <nickesparza@gmail.com>
2022-07-28 19:45:48 -0500

    add team_routes to server.js and test index and show routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const teamRoutes = require('./app/routes/team_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -65,6 +66,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(requestLogger)
 
 // register route files
+app.use(teamRoutes)
 app.use(exampleRoutes)
 app.use(userRoutes)
 
